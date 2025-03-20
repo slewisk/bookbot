b39f919faa54e26c1f2f0041cd3a2ef2fc6c3333
slewisk <slewisk@gmail.com>
2025-03-20 15:50:50 -0400

    completed bookbot project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import book_word_count, character_count, sort_on, create_dict_list, print_dict_list_clean
 from string import ascii_lowercase
+import sys
 def get_book_text(filepath):
     with open(filepath) as file:
         book_text = file.read()
@@ -7,6 +8,11 @@ def get_book_text(filepath):
 
 
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path = sys.argv[1]
     word_count = book_word_count(get_book_text(path))
     char_count = character_count(get_book_text(path))
     char_list = create_dict_list(char_count)
